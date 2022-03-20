e9a6b7c01457a7833401bbec720ad80e2011e561
jimohsegun <jimohsegunj@gmail.com>
2022-03-20 13:02:35 -0700

    commit

--- 0x04-more_functions_ned_loops/4-print_most_numbers.c
+++ 0x04-more_functions_ned_loops/4-print_most_numbers.c
@@ -7,17 +7,17 @@
 void print_most_numbers(void)
 {
 
-  int i;
+int i;
 
-  i = 0;
-  while (i < 10)
-    {
-      if (i != 2 && i != 4)
-	{
-	  _putchar (i + '0');
-	}
-      i++;
-    }
+i = 0;
+while (i < 10)
+{
+if (i != 2 && i != 4)
+{
+_putchar (i + '0');
+}
+i++;
+}
 
-  _putchar ('\n');
+_putchar ('\n');
 }
