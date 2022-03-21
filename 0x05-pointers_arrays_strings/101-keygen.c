b659807b0f0cbe808cce3f3f1255bd6de1497157
jimohsegun <jimohsegunj@gmail.com>
2022-03-21 14:23:51 -0700

    commit

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -3,6 +3,10 @@
 #include <time.h>
 #include <stdio.h>
 /**
+ * main - program that generates random valid
+ * passwords for the program 101-crackme
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
