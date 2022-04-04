a34a98e30717147a0810bd99608fed427fca6830
jimohsegun <jimohsegunj@gmail.com>
2022-04-04 15:42:33 -0700

    commit

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,55 +1,62 @@
-#include <stdlib.h>
 #include "main.h"
-
 /**
- * _strlen - returns the length of a string
- * @s: string s
- 
- * Return: length of string
+ *_strlen - count arrray
+ *@s: array of elements
+ *Return: i
  */
+
 int _strlen(char *s)
 {
-int length = 0;
+unsigned int i;
 
-while (*s)
+i = 0;
+while (s[i] != '\0') /*Count character of string*/
 {
-s++;
-length++;
+i++;
 }
-return (length);
+
+return (i);
 }
 
 /**
- * str_concat - concatenates two strings
- * @s1: first string
- * @s2: second string
- * Return: concatenated strings
+ *str_concat - back a pointer to array
+ *@s1: Array one
+ *@s2: Array two
+ *Return: Always an array dinamic
  */
+
 char *str_concat(char *s1, char *s2)
 {
-char *cat, *_cat;
+char *dst;
+unsigned int i, j, size;
 
+/*If the array is empty*/
 if (s1 == NULL)
 s1 = "";
+
 if (s2 == NULL)
 s2 = "";
 
-cat = malloc(sizeof(char) * (_strlen(s1) + _strlen(s2)) + 1);
-if (!cat)
+/*count size total*/
+size = (_strlen(s1) + _strlen(s2) + 1);
+
+/*malloc*/
+dst = (char *) malloc(size * sizeof(char));
+
+if (dst == 0)
+{
 return (NULL);
-_cat = cat;
-while (*s1)
-{
-*_cat = *s1;
-_cat++;
-s1++;
 }
-while (*s2)
+
+/*Concatenate arrays*/
+for (i = 0; *(s1 + i) != '\0'; i++)
+*(dst + i) = *(s1 + i);
+
+for (j = 0; *(s2 + j) != '\0'; j++)
 {
-*_cat = *s2;
-_cat++;
-s2++;
+*(dst + i) = *(s2 + j);
+i++;
 }
-*_cat = '\0';
-return (cat);
+
+return (dst);
 }
