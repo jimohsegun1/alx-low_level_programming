ede4d6b0c9d572b625f407a72a24091744cf58e4
jimohsegun <jimohsegunj@gmail.com>
2022-04-07 04:22:08 -0700

    commit

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,128 +1,143 @@
+#include <stdlib.h>
 #include "main.h"
 
 /**
- * main - multiplies two positive numbers
- * @argc: argument count
- * @argv: argument vectors
- * Return: 0
+ * _prntstr - prints a string
+ *
+ * @s: string to print
  */
-int main(int argc, char *argv[])
+void _prntstr(char *s)
 {
-	char *f = argv[1];
-	char *s = argv[2];
+	while (*s)
+		_putchar(*s++);
+}
 
-	if (argc != 3 || !onlyNumbers(f) || !onlyNumbers(s))
+/**
+ * numstrchk - checks arg array to see if the are numeric strings, converts
+ * from ascii to byte int, and returns their length. Segfault on null pointer.
+ *
+ * @s: string to check
+ *
+ * Return: Length of string. Exit 98 if not numeric.
+ */
+long int numstrchk(char *s)
+{
+	long int len = 0;
+
+	if (*s == 0)
 	{
-		printf("Error\n");
+		_prntstr("Error\n");
 		exit(98);
 	}
-	if (*f == 48 || *s == 48)
-		printf("0\n");
-	else
-		multiply(s, f);
-	return (0);
-}
-
-/**
- * multiply - multiplies two numbers and displays it
- * @f: first "number"
- * @s: second "number"
- */
-void multiply(char *f, char *s)
-{
-	int i, len1, len2, total, fdigit, sdigit, res = 0, tmp;
-	int *ptr;
-
-	len1 = _strlen(f);
-	len2 = _strlen(s);
-	tmp = len2;
-	total = len1 + len2;
-	ptr = _calloc(sizeof(int), (len1 + len2));
-	for (len1--; len1 >= 0; len1--)
-	{
-		fdigit = f[len1] - '0';
-		res = 0;
-		len2 = tmp;
-		for (len2--; len2 >= 0; len2--)
-		{
-			sdigit = s[len2] - '0';
-			res += ptr[len2 + len1 + 1] + (fdigit * sdigit);
-			ptr[len1 + len2 + 1] = res % 10;
-			res /= 10;
-		}
-		if (res)
-			ptr[len1 + len2 + 1] = res % 10;
-	}
-	while (*ptr == 0)
-	{
-		ptr++;
-		total--;
-	}
-	for (i = 0; i < total; i++)
-		printf("%i", ptr[i]);
-	printf("\n");
-}
-/**
- * onlyNumbers - determines if string has only numbers
- * @c: input string
- * Return: 0 if false, 1 if true
- */
-int onlyNumbers(char *c)
-{
-	while (*c)
-	{
-		if (*c < '0' || *c > '9')
-			return (0);
-		c++;
-	}
-	return (1);
-}
-
-/**
- * _strlen - returns the length of a string
- * @s: string s
- * Return: length of string
- */
-int _strlen(char *s)
-{
-	char *p = s;
 
 	while (*s)
+	{
+		if (*s < '0' || *s > '9')
+		{
+			_prntstr("Error\n");
+			exit(98);
+		}
+		*s -= '0';
+		len++;
 		s++;
-	return (s - p);
+	}
+	return (len);
 }
 
 /**
- * _memset - fills memory with a constant byte
- * @s: memory area
- * @b: constant byte
- * @n: bytes of the memory area
- * Return: pointer to the memory area s
+ * _calloc_buffer - allocate a block of memory of size * num and init to '0'
+ *
+ * @num: number of elements to allocate
+ * @size: size of element
+ *
+ * Return: pointer to allocated space, exit 98 on failure
  */
-char *_memset(char *s, char b, unsigned int n)
+void *_calloc_buffer(long int num, long int size)
 {
-	char *ptr = s;
+	void *ret;
+	char *ptr;
 
-	while (n--)
-		*s++ = b;
-	return (ptr);
+	ret = malloc(num * size);
+	if (ret == 0)
+	{
+		exit(98);
+	}
+
+	size = size * num;
+	ptr = ret;
+	ptr[--size] = 0;
+	while (size--)
+		ptr[size] = '0';
+
+	return (ret);
 }
 
 /**
- * _calloc - allocates memory for an array, using malloc
- * @nmemb: number of elements of pointer
- * @size: size of each member
- * Return: pointer of allocated memory
+ * trimzero - moves pointer position to after last leading 0 in a string,
+ * or last zero if all zeros
+ *
+ * @s: char * we want to move
+ *
+ * Return: new position
  */
-void *_calloc(unsigned int nmemb, unsigned int size)
+char *trimzero(char *s)
 {
-	void *ptr;
+	while (*s == '0')
+		if (*(s + 1) != 0)
+			s++;
+		else
+			break;
+	return (s);
+}
 
-	if (!nmemb || !size)
-		return (NULL);
-	ptr = malloc(size * nmemb);
-	if (!ptr)
-		return (NULL);
-	_memset(ptr, 0, size * nmemb);
-	return (ptr);
+/**
+ * main - multiply two  positive integer strings of arbitrary size
+ *
+ * @ac: number of arguments
+ * @av: arugments
+ *
+ * Return: 0 if successful, 98 if failure
+ */
+int main(int ac, char **av)
+{
+	long int len1, len2, lenres, i, j;
+	char *res;
+
+	if (ac != 3)
+	{
+		_prntstr("Error\n");
+		return (98);
+	}
+	av[2] = trimzero(av[2]);
+	av[1] = trimzero(av[1]);
+	if (*av[1] == '0' || *av[2] == '0')
+	{
+		_prntstr("0\n");
+		return (0);
+	}
+	len1 = numstrchk(av[1]);
+	len2 = numstrchk(av[2]);
+	lenres = len1 + len2;
+	res = _calloc_buffer(lenres + 1, sizeof(char));
+
+	for (i = lenres - 1, len1--; len1 >= 0; len1--, i += len2 - 1)
+		for (j = len2 - 1; j >= 0; j--, i--)
+		{
+			res[i] = (av[1][len1] * av[2][j] % 10) + res[i];
+			res[i - 1] = (av[1][len1] * av[2][j] / 10) + res[i - 1];
+			if (res[i] > '9')
+			{
+				res[i] -= 10;
+				res[i - 1]++;
+			}
+		}
+
+	if (*res == '0')
+		_prntstr(res + 1);
+	else
+		_prntstr(res);
+	_putchar('\n');
+	free(res);
+
+	return (0);
 }
